44d2dd77a18ffe299b332d6c2d6ddfb92866e784
Ferry Manders <ferrymanders@users.noreply.github.com>
2019-01-14 09:20:01 +0100

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ echo "# Move site files to docroot"
 cp -r /tmp/src/site/* /var/www/html/
 
 echo "# place pre-startup script"
+[ -d /var/www/cmd ] || mkdir /var/www/cmd
 cp /tmp/src/scripts/pre-run  /var/www/cmd/pre-run
